b6cad72b0d9c295036c68e37430c8262b3cae25e
c0nor-S <150271462+c0nor-S@users.noreply.github.com>
2026-02-22 20:47:54 +0000

    First Commit

---
+++ src/test/java/ie/atu/sem2/week2/sem2week4/Sem2Week4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.sem2.week2.sem2week4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2Week4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
